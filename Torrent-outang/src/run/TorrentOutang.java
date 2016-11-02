25334deb499ff7849cf617b7f8433bb189c7530f
Aymeric <lesne.aymeric@gmail.com>
2016-11-02 10:12:16 +0100

    Ajout dépendance log4j

--- Torrent-outang/src/run/TorrentOutang.java
+++ Torrent-outang/src/run/TorrentOutang.java
@@ -9,14 +9,21 @@ import tool.MakeTorrent;
 
 public class TorrentOutang {
 	
+	public static void usage(){
+		System.err.println("usage: create <folder> creates and seeds torrent from that folder");
+		System.err.println("usage: download <.torrent file> downloads torrent in actual directory");
+		System.exit(0);
+	}
+	
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BasicConfigurator.configure();
 		if(args.length!=2){
-			System.err.println("usage: create <folder> creates and seeds torrent from that folder");
-			System.err.println("usage: download <.torrent file> downloads torrent in actual directory");
+			usage();
+		}
+		if(args.length==0){
 			
 		}
 		if(args[0].equals("create")){
@@ -28,9 +35,7 @@ public class TorrentOutang {
 				else{
 					if(dossier.isFile()){
 						System.err.println("No file, only folders");
-						System.err.println("usage: create <folder> creates and seeds torrent from that folder");
-						System.err.println("usage: download <.torrent file> downloads torrent in actual directory");
-						System.exit(0);
+						usage();
 					}
 					if(dossier.list().length==0){
 						System.err.println("Directory is empty");
@@ -47,8 +52,7 @@ public class TorrentOutang {
 				new GetTorrent(fichier_tor);
 			}else{
 				System.err.println("problem with .torrent file!");
-				System.err.println("usage: create <folder> creates and seeds torrent from that folder");
-				System.err.println("usage: download <.torrent file> downloads torrent in actual directory");
+				usage();
 			}
 			
 		}
