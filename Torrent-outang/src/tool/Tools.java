050758acbd7cff528c875b1049b98b01ebb4ecad
Aymeric <lesne.aymeric@gmail.com>
2016-11-02 10:29:45 +0100

    Correction tools, ne rajoute plus deux fois le fichier dans la liste...

--- Torrent-outang/src/tool/Tools.java
+++ Torrent-outang/src/tool/Tools.java
@@ -10,15 +10,8 @@ import org.apache.commons.io.filefilter.TrueFileFilter;
 public class Tools {
 
 	public List <File> toList(File dossier){
-		List<File> liste = new ArrayList<File>(FileUtils.listFiles(dossier, TrueFileFilter.TRUE, TrueFileFilter.TRUE));
-		//FileFilter ff=new FileFilter();
+		List<File> liste = new ArrayList<File>(FileUtils.listFiles(dossier, TrueFileFilter.TRUE, TrueFileFilter.TRUE ));
 		
-		String[]tableau=dossier.list();
-		
-		for (int i=0;i<tableau.length;i++){
-			liste.add(new File(dossier.getAbsolutePath()+ File.separator+tableau[i]));
-			System.out.println("tab(i):" + tableau[i]);
-		}
 		return liste;
 	}
 }
